27b1542d44ffebffa8983d1772cfa76e10709caf
yasminaad <jasmina031196@gmail.com>
2022-07-19 16:56:05 +0200

    firstereact

---
+++ firstapp/src/App.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="one">
+       <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label className="mb-1">Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className="mb-1">Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check className="btn" type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+  );
+}
+
+export default App;
